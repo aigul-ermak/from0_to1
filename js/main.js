b49808dc96116b71d20990b38e3fbfd027e70504
aigul <aigul.erma@gmail.com>
2022-01-11 21:31:42 -0400

    lesson 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+$(function () {
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="./images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./images/next.png" alt=""></button>'
+    })
 });
