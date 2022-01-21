7f2a2463268147dbc38272bb77855b4f9c9e189f
aigul <aigul.erma@gmail.com>
2022-01-21 18:19:10 -0400

    lesson 2 continue

--- js/main.js
+++ js/main.js
@@ -4,4 +4,25 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"> <img src="./images/prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="./images/next.png" alt=""></button>'
     })
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+
+        prevArrow: '<button type="button" class="product-prev"> <img src="./images/product-prev.png" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="./images/product-next.png" alt=""></button>'
+    })
+
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true
+    })
 });
+
+
+
